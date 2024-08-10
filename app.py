21141d8d57272e7ba1a483d0da7a428346b548df
Danilo <Danilo@gavari.com>
2024-08-09 22:37:20 -0300

    First Commit

---
+++ app.py
@@ -0,0 +1,111 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__) 
+
+# Configuracion de SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/efi_python'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Marca, Tipo, Telefono, Accesorio, Telefono_Accesorio
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/marca_list", methods=['POST', 'GET'])
+def marcas():   
+    marcas = Marca.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nueva_marca = Marca(nombre=nombre)
+        db.session.add(nueva_marca)
+        db.session.commit()
+        return redirect(url_for('marcas'))
+
+    return render_template('marca_list.html',
+                marcas=marcas)
+
+@app.route("/marca/<id>/telefono")
+def telefonos_por_marca(id):
+    marca = Marca.query.get_or_404(id)
+    telefonos = marca.telefonos
+    return render_template("telefonos_by_marca.html", telefonos=telefonos, marca=marca)
+
+
+@app.route("/marca/<id>/editar", methods=['GET', 'POST'])
+def marca_editar(id):
+    marca = Marca.query.get_or_404(id)
+
+    if request.method == 'POST':
+        marca.nombre = request.form['nombre']
+        db.session.commit()
+        return redirect(url_for('marcas'))
+
+    return render_template("marca_edit.html", marca=marca)
+
+@app.route("/tipo_list", methods=['GET', 'POST'])
+def tipos():
+    tipos = Tipo.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nuevo_tipo = Tipo(nombre=nombre)
+        db.session.add(nuevo_tipo)
+        db.session.commit()
+        return redirect(url_for('tipos'))
+
+    return render_template('tipo_list.html', tipos=tipos)
+
+
+@app.route("/telefono_list", methods=['POST', 'GET'])
+def telefonos():
+    telefonos = Telefono.query.all()
+    marcas = Marca.query.all()
+    tipos = Tipo.query.all()
+
+    if request.method == 'POST':
+        modelo = request.form['modelo']
+        anio = request.form['anio_fabricacion']
+        precio = request.form['precio']
+        marca = request.form['marca']
+        tipo = request.form['tipo']
+        telefono_nuevo = Telefono(
+            modelo=modelo,
+            anio_fabricacion=anio,
+            precio=precio,
+            marca_id=marca,
+            tipo_id=tipo,
+        )
+        db.session.add(telefono_nuevo)
+        db.session.commit()
+        return redirect(url_for('telefonos'))
+
+    return render_template('telefono_list.html', telefonos=telefonos, marcas=marcas, tipos=tipos)
+
+
+@app.route("/accesorios_list", methods=["GET", "POST"])
+def accesorios():
+    accesorios = Accesorio.query.all()
+
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        nuevo_accesorio = Accesorio(nombre=nombre)
+        db.session.add(nuevo_accesorio)
+        db.session.commit()
+        return redirect(url_for('accesorios'))
+
+    return render_template('accesorios_list.html', accesorios=accesorios)
+
+@app.route("/telefono/<id>", methods=['GET'])
+def telefono_accesorio(id):
+    telefono = Telefono.query.get_or_404(id)
+    accesorios = [ta.accesorio for ta in Telefono_Accesorio.query.filter_by(telefono_id=id).all()]
+    return render_template("accesorios_by_telefono.html", telefono=telefono, accesorios=accesorios)
+
---
+++ models.py
@@ -0,0 +1,48 @@
+from app import db
+
+class Marca(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Tipo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+
+    def __str__(self) -> str:
+        return f"Tipo {self.nombre}"
+
+
+class Telefono(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    modelo = db.Column(db.String(50), nullable=False)
+    anio_fabricacion = db.Column(db.Integer)
+    precio = db.Column(db.Integer)
+    marca_id = db.Column(db.Integer, db.ForeignKey('marca.id'), nullable=False)
+    tipo_id = db.Column(db.Integer, db.ForeignKey('tipo.id'), nullable=False)
+
+    #Relacion directa con el otro objeto
+    marca = db.relationship('Marca', backref=db.backref('telefono', lazy=True))
+    tipo = db.relationship('Tipo', backref=db.backref('telefono', lazy=True))
+    accesorios = db.relationship('Telefono_Accesorio', backref=db.backref('telefono',lazy=True))
+
+    def __str__(self) -> str:
+        return f"Telefono {self.modelo}"
+    
+
+class Accesorio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(50), nullable=False)
+    telefonos = db.relationship('Telefono_Accesorio', backref=db.backref ('accesorios', lazy=True))
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Telefono_Accesorio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    telefono_id = db.Column(db.Integer, db.ForeignKey('telefono.id'), nullable=False)
+    accesorio_id = db.Column(db.Integer, db.ForeignKey('accesorio.id'), nullable=False)
+
+    accesorio = db.relationship('Accesorio', backref=db.backref ('telefono_accesorio', lazy=True))
